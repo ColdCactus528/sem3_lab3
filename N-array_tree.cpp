dc79815eb09c025c5833b087cbc27551f064abf9
ColdCactus528 <56156478+ColdCactus528@users.noreply.github.com>
2021-07-14 16:09:45 +0300

    Update N-array_tree.cpp

--- N-array_tree.cpp
+++ N-array_tree.cpp
@@ -48,7 +48,6 @@ public:
 					std::vector<std::vector<R_object>> massive = array;
 					int erase_count;
 					erase_count = j;
-					std::cout << "Erase = " << erase_count << "\n";
 
 					massive.erase(massive.begin() + j,massive.begin() + j + 1);
 					FillTree(new_node, massive);	
@@ -81,6 +80,7 @@ public:
 		delete root;
 	}
 
+	#ifdef DEBUG 
 	void PrintNodes(tnode<Rucksack>* node) {
 		for (int i = 0; i < node->children.size(); i ++) {
 			for (int j = 0; j < node->children[i]->data.GetMatrix().size(); j ++) {
@@ -93,82 +93,16 @@ public:
 			PrintNodes(node->children[i]);
 		}
 	}
+	#endif
 
-	void GetAllLevesBySequnce(std::vector<std::vector<Rucksack>& Rucksack_array) {
-		
+	void GetAllLevesBySequnce(tnode<Rucksack>* node, std::vector<Rucksack>& Rucksack_array) {
+		if (node->children.size() < 1) 
+			Rucksack_array.push_back(node->data); 
+
+		for (int i = 0; i < node->children.size(); i ++) {
+				GetAllLevesBySequnce(node->children[i], Rucksack_array);
+		}
 	}
 private:
 	tnode<T>* root;
 };
-
-int main() {
-	Rucksack R;
-	R_object object_0;
-	R_object object_1;
-	R_object object;
-	std::vector<R_object> array_of_objects_0;
-	std::vector<R_object> array_of_objects_1;
-	std::vector<std::vector<R_object>> array;
-	int size_matrix = 3;
-	std::vector<std::vector<int>> matrix(size_matrix, std::vector<int> (size_matrix));     
-
-    // задание матрицы
-    int z = 0;
-    for (int i = 0; i < size_matrix; i++) {
-        for (int j = 0; j < size_matrix; j++) 
-            matrix[i][j] = z;  
-    }
-
-	R.Set(matrix);
-	object_0.SetParametres(0, 3, 4, matrix);
-    matrix[1][1] = 1;
-    // конец задания матрицы
-
-	object_0.SetParametres(4, 3, 4, matrix);
-	std::cout << "First object :\n";
-	for (int i = 0; i < size_matrix; i++) {
-        for (int j = 0; j < size_matrix; j++) 
-            std::cout << object_0.GetMatrix()[i][j] << " ";   
-        std::cout << "\n"; 
-    }
-    std::cout << "\n";
-
-	object_1.SetParametres(3, 3, 2, matrix);
-	std::cout << "Second object :\n";
-	for (int i = 0; i < size_matrix; i++) {
-        for (int j = 0; j < size_matrix; j++) 
-            std::cout << object_1.GetMatrix()[i][j] << " ";   
-        std::cout << "\n"; 
-    }
-    std::cout << "\n";
-
-	array_of_objects_0.push_back(object_0);
-	MakeSequence_of_one_object(array_of_objects_0, object_0);
-	array_of_objects_1.push_back(object_1);
-	MakeSequence_of_one_object(array_of_objects_1, object_1);
-
-	array.push_back(array_of_objects_0);
-	array.push_back(array_of_objects_1);
-
-	for (int k = 0; k < array.size(); k ++) {
-		for (int l = 0; l < array[k].size(); l++) {
-			for (int i = 0; i < size_matrix; i++) {
-				for (int j = 0; j < size_matrix; j++) 
-					std::cout << array[k][l].GetMatrix()[i][j] << " ";   
-				std::cout << "\n"; 
-			}
-			std::cout << "\n";
-		}	
-	}
-    N_array_tree<Rucksack> tree (R);
-
-	std::cout << "\nhang yourself at home\n";
-
-	tree.FillTree(tree.GetRoot(), array);
-	tnode<Rucksack>* node = tree.GetRoot();
-
-	std::cout << "Yep\n";
-	tree.PrintNodes(node);
-
-	return 0;
-}
